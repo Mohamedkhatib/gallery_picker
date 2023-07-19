7acd2d3846b579f48c77cebb2b354ef6ab2ce25f
Mohamedkhatib <39992807+Mohamedkhatib@users.noreply.github.com>
2023-07-20 00:45:49 +0300

    fix arabic numbers

--- lib/models/gallery_album.dart
+++ lib/models/gallery_album.dart
@@ -165,11 +165,22 @@ class DateCategory {
       ...config.months
     ].indexOf(name);
     if (index == -1) {
-      return 3000 - int.parse(name);
+      return 3000 - int.parse(replaceFarsiNumber (name));
     } else {
       return index;
     }
   }
+}
+
+String replaceFarsiNumber(String input) {
+  const english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const farsi = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+  for (int i = 0; i < english.length; i++) {
+    input = input.replaceAll(farsi[i],english [i]);
+  }
+
+  return input;
 }
 
 enum AlbumType { video, image, mixed }
